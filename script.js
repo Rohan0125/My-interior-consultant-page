d83a03599dbb39bcbcc691c62dbcb1723b80e608
rohan <rohanghodke1250@gmail.com>
2021-12-28 14:46:20 +0530

    project complete

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function myFunction() {
+
+  var navs = document.getElementById("navbar");
+
+  if(navs.style.display === "flex"){
+    navs.style.display = "none";
+  }
+  else{
+    navs.style.display="flex";
+  }
+
+}
